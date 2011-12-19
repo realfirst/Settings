6e95d9745f2f40c448b787be70d61756dae3f9df
Liu Yen-Liang (josh) <yenliangl@gmail.com>
2011-12-19 21:04:39 +0800

    change chinese font automatically based on ns-antialias-text

--- Emacs/24.el
+++ Emacs/24.el
@@ -16,13 +16,25 @@
 ;; --------------------------------------------------
 ;; Color theme
 ;; --------------------------------------------------
-;; (add-hook 'after-make-window-system-frame-hooks
-;;           (lambda ()
-;;             ;; (load-theme 'tsdh-dark)))
-;;             ;; (load-theme 'tango-dark)))
-;;             ;; (load-theme 'tsdh-dark)))
-;;             (load-theme 'manoj-dark)
-;;             ;; (load-theme 'wombat)
-;;             ;; (load-theme 'tango)
-;;             ;; (load-theme 'whiteboard)
-;;             ))
+(add-hook 'after-make-window-system-frame-hooks
+          (lambda ()
+            ;; (load-theme 'tsdh-dark)
+            (load-theme 'tango-dark)
+            ;; (load-theme 'tsdh-dark)
+            ;; (load-theme 'manoj-dark)
+            ;; (load-theme 'wombat)
+            ;; (load-theme 'tango)
+            ;; (load-theme 'whiteboard)
+            ))
+
+;; --------------------------------------------------
+;; Org-mode
+;; --------------------------------------------------
+;; (message ">>>>> [Emacs23] Setting load-path for org-mode <<<<<")
+;; (setq ORG_LISP_HOME (concat ELPA_HOME "/org-20111026"))
+(setq ORG_LISP_HOME (concat LISP_HOME "/org/org-7.8.02"))
+;; (setq ORG_LISP_HOME (concat LISP_HOME "/org/org-7.7"))
+
+;; (add-to-list load-path (concat LISP_HOME "/remember"))
+(add-to-list 'load-path (concat ORG_LISP_HOME "/lisp"))
+(add-to-list 'load-path (concat ORG_LISP_HOME "/contrib/lisp"))
