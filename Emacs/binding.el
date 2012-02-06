778bc0674559c1c185bb9cad331e2b1ecc17ae1f
Liu Yen-Liang (josh) <yenliangl@gmail.com>
2012-02-06 16:16:20 +0800

    Add smart-beginning-of-line to move cursor to first non-whitespace char

--- Emacs/binding.el
+++ Emacs/binding.el
@@ -18,6 +18,16 @@
   (kill-buffer (current-buffer)))
 (global-set-key [f12] 'josh/kill-current-buffer)
 
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character or beginning-of-line. Move point to the first non-whitespace character on this line.
+If point was already at that position, move point to beginning of
+line."
+  (interactive) ; Use (interactive "^") in Emacs 23 to make shift-select work
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+         (beginning-of-line))))
+(global-set-key [home] 'smart-beginning-of-line)
 
 (global-set-key [insertchar] 'overwrite-mode)
 
